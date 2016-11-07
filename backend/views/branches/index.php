b13d23617ff5a9112294c4a0fea120bf817ebf50
Clave Quimbo <claveeq@gmail.com>
2016-11-07 11:37:24 +0800

    fix related table data from the grid view

--- backend/views/branches/index.php
+++ backend/views/branches/index.php
@@ -23,9 +23,11 @@ $this->params['breadcrumbs'][] = $this->title;
         'filterModel' => $searchModel,
         'columns' => [
             ['class' => 'yii\grid\SerialColumn'],
-
+            [
+            'attribute' => 'companies_company_id',
+            'value' => 'companiesCompany.company_name'
+            ],
             // 'branch_id',
-            'companiesCompany.company_name',
             'branch_name',
             'branch_address',
             'branch_created_date',
